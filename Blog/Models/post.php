399e1a2d117631c050716cb30561458a91359c31
unaialberdi22 <72130063+unaialberdi22@users.noreply.github.com>
2022-10-17 12:05:02 +0200

    Add files via upload

---
+++ Blog/Models/post.php
@@ -0,0 +1,152 @@
+<?php
+
+class post {
+    private $id;
+    private $title;
+    private $brief;
+    private $content;
+    private $image;
+    private $created_at;
+    private $status;
+    private $user_id;
+    
+    function __construct(string $id, $title, $brief, $content, $image, $created_at, $status, $user_id){
+        $this->id = $id;
+        $this->title = $title;
+        $this->brief = $brief;
+        $this->content = $content;
+        $this->image = $image;
+        $this->created_at = $created_at;
+        $this->status = $status;
+        $this->user_id = $user_id;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBrief()
+    {
+        return $this->brief;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreated_at()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser_id()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @param mixed $brief
+     */
+    public function setBrief($brief)
+    {
+        $this->brief = $brief;
+    }
+
+    /**
+     * @param mixed $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @param mixed $image
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * @param mixed $created_at
+     */
+    public function setCreated_at($created_at)
+    {
+        $this->created_at = $created_at;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @param mixed $user_id
+     */
+    public function setUser_id($user_id)
+    {
+        $this->user_id = $user_id;
+    }
+    
+}
