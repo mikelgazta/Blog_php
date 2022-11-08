e11959318f93e913c5fb41d2e9c5069c004176f2
Mikel <91115057+mikelgazta@users.noreply.github.com>
2022-11-08 10:16:14 +0100

    Add files via upload

---
+++ Blog/System/C_UpdateUser.php
@@ -0,0 +1,28 @@
+<?php
+
+use Models\M_User;
+use Models\User;
+require_once '../Models/User.php';
+require_once '../Models/M_user.php';
+
+$id = $_POST['id'];
+$name = $_POST['name'];
+$lastname = $_POST['lastname'];
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$image = $_POST['image'];
+$status = $_POST['status'];
+$kind = $_POST['kind'];
+$created_at = $_POST['created_at'];
+
+$con = new M_User();
+
+$user = new User($id, $name, $lastname, $username, $email, $password, $image, $status, $kind, $created_at);
+
+$con->updateUser($user);
+$con->close();
+
+header("Location: ../Views/V_SelectUser.php");
+exit();
+?>
